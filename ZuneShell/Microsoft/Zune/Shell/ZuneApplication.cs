4b85f01f18c632ad05cf017543204ccd17a8b7bd
Gustave Monce <gustave.monce@outlook.com>
2023-05-11 14:55:14 +0200

    Fix up csproj for my own use

--- ZuneShell/Microsoft/Zune/Shell/ZuneApplication.cs
+++ ZuneShell/Microsoft/Zune/Shell/ZuneApplication.cs
@@ -150,7 +150,7 @@ namespace Microsoft.Zune.Shell
             cacheFolderPath.Create();
             OwlCore.Storage.SystemIO.SystemFolder cacheFolder = new(cacheFolderPath);
 
-            OwlCore.Storage.SystemIO.SystemFolder musicFolder = new(@"D:\Music\Zune\Test");
+            OwlCore.Storage.SystemIO.SystemFolder musicFolder = new(@"C:\Music\Zune\Test");
             var localCore = new StrixMusic.Cores.Storage.StorageCore(musicFolder, cacheFolder, "Local Test")
             {
                 ScannerWaitBehavior = StrixMusic.Cores.Storage.ScannerWaitBehavior.AlwaysWait,
